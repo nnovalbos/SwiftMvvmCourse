b546b60cf0e3cf2f6f45866f0cb328838b9a2c95
Nico <nnovalbos.inf@upsa.es>
2020-04-05 08:41:41 +0200

    Se añaden binding: View to ViewModel

--- GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -15,11 +15,38 @@ protocol AddWeatherDelegate {
 
 class AddWeatherCityViewController: UIViewController {
     
-    @IBOutlet weak var cityName : UITextField!
+    private var addCityVieModel = AddCityViewModel()
+    
+    @IBOutlet weak var cityName : BindingTextField! {
+        
+        didSet {
+            
+            cityName.bind { self.addCityVieModel.city = $0}
+        }
+    }
+    
+    @IBOutlet weak var stateName : BindingTextField! {
+        didSet {
+            
+            stateName.bind { self.addCityVieModel.state = $0}
+        }
+    }
+    @IBOutlet weak var zipCodeName : BindingTextField! {
+        didSet {
+            
+            zipCodeName.bind { self.addCityVieModel.zipCode = $0}
+        }
+    }
+    
+    
+    
     var delegate : AddWeatherDelegate?
     let API_KEY = "YOUR_API_KEY"
     
     @IBAction func Save(){
+        
+        print(addCityVieModel)
+        
         if let cityName = cityName.text {
             let unit = UserDefaults.standard.value(forKey: "unit") as! String
             let weatherURL = URL (string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&APPID=\(API_KEY)&units=\(unit)")!
---
+++ GoodWeather/GoodWeather/ViewModels/AddCityViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AddCityViewModel.swift
+//  GoodWeather
+//
+//  Created by Nicolas Novalbos on 04/04/2020.
+//  Copyright © 2020 Nicolas Novalbos. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AddCityViewModel {
+ 
+    var city: String = ""
+    var state: String = ""
+    var zipCode: String = ""
+    
+}
